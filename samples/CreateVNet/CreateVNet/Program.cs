6c6b81097185b03130da9ff694c6c8d8f73b85f0
Chun Liu <chunliu@microsoft.com>
2022-05-01 13:42:31 +0800

    Update readme and samples

---
+++ samples/CreateVNet/CreateVNet/Program.cs
@@ -0,0 +1,47 @@
+﻿using AzureDesignStudio.AzureResources.Base;
+using AzureDesignStudio.AzureResources.Network;
+using CreateVNet;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+VirtualNetworks vnet = new()
+{
+    Name = "Hello-vnet",
+    Location = "eastus",
+    Properties = new()
+    {
+        AddressSpace = new()
+        {
+            AddressPrefixes = new List<string> { "10.0.0.0/16" }
+        }
+    }
+};
+
+VirtualNetworksSubnets subnet = new()
+{
+    Name = $"{vnet.Name}/subnet1",
+    Properties = new()
+    {
+        AddressPrefix = "10.0.0.0/24"
+    },
+};
+
+DeploymentTemplate deploymentTemplate = new()
+{
+    Schema = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    ContentVersion = "1.0.0.0",
+    Resources = new List<ResourceBase> { vnet, subnet }
+};
+
+var template = JsonSerializer.Serialize(deploymentTemplate,
+    new JsonSerializerOptions(JsonSerializerDefaults.Web)
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        WriteIndented = true,
+        Converters = { new ResourceBaseJsonConverter() }
+    });
+
+Console.Write(template);
+
